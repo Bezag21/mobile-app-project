8ea0045ebe778e3dd4ea67baa86560d94a463807
Bezag21 <120325853+Bezag21@users.noreply.github.com>
2023-05-26 00:16:03 +0300

    Add files via upload

---
+++ Property-Management/Back/src/models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const { bool } = require('joi');
+
+// Define Schema
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    roles: {
+        type: String,
+        required: true,
+        default: 'user',
+        enum: ['user', 'admin']
+    },
+
+     
+
+});
+
+// Hash password before save in DB
+userSchema.statics.encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10)
+    return await bcrypt.hash(password, salt)
+};
+
+
+// Compare password 
+userSchema.statics.comparePassword = async (password, receivedPassword) => {
+    return await bcrypt.compare(password, receivedPassword)
+};
+
+
+module.exports = mongoose.model('User', userSchema);
