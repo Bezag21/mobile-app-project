45430b52a15e9fe20409d87e20ac3653aaaf539c
Meti <119840944+Meti8202@users.noreply.github.com>
2023-06-02 23:09:42 +0300

    Create endpoint.dart

---
+++ Property-Management/Front/lib/infrastructure/endpoints/endpoint.dart
@@ -0,0 +1,15 @@
+class EndPoint {
+  String baseUrl ='http://192.168.56.1:3000/api/';// 'http://192.168.1.6:3000/api/';
+  String auth = 'auth';
+  String login = 'login';
+  String signup = 'signup';
+  String profile = 'profile';
+  String logout = 'logout';
+  String inspection='inspection';
+  String upload= 'upload';
+  // String getInspection='get_inspection';
+  // String allInspection='all_inspection';
+  // String addInspection='add_inspection';
+  //  String editInspection='edit_inspection';
+  //  String deleteInspection='delete_inspection';
+}
