9724a7e1a1edd1a02477f37c07bb4c960ece3071
Birhan_Aschalew <107642425+Birhanachalew@users.noreply.github.com>
2023-06-02 23:32:32 +0300

    Create text_data.dart

---
+++ Property-Management/Front/lib/presentation_data/text_data.dart
@@ -0,0 +1,6 @@
+class TextData {
+  String introTitle = "Explore the app";
+  String introDetails =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maecenas quis interdum enim enim molestie faucibus. Pretium non non massa eros, nunc, urna. Ac laoreet sagittis donec vel. Amet, duis justo, quam quisque egestas. Quam enim at dictum condimentum. Suspendisse.";
+  
+}
