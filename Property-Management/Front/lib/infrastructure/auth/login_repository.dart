aaa944a44f28f4bacc6417d8e6bda6edbbd8960e
Meti <119840944+Meti8202@users.noreply.github.com>
2023-06-02 23:06:09 +0300

    Create login_repository.dart

---
+++ Property-Management/Front/lib/infrastructure/auth/login_repository.dart
@@ -0,0 +1,29 @@
+import '../../../domain/auth/loggedIn_domain.dart';
+import '../../../domain/auth/login_user_domain.dart';
+import '../../../domain/auth/role_domain.dart';
+import '../../../domain/auth/token.dart';
+import '../../../domain/auth/user_name_domain.dart';
+import '../../../infrastructure/auth/login_model.dart';
+import '../../../infrastructure/auth/login_data_provider.dart';
+import '../../../infrastructure/auth/login_response_model.dart';
+
+class LoginRepo {
+  LoginRepo();
+  AuthDataProvider authDataProvider = AuthDataProvider();
+
+  Future<LoggedInDomain> login(LoginDomain loginDomain) async {
+    LoginUser loginUser = LoginUser(
+        username: loginDomain.username.toString(),
+        password: loginDomain.password.toString());
+    print(loginUser.username);
+    print(loginUser.password);
+    LoginResponse response = await authDataProvider.login(loginUser);
+    print(response.role);
+    LoggedInDomain loggedInDomain = LoggedInDomain(
+        token: Token(token: response.token),
+        username: Username(username: response.name),
+        role: Role(role: response.role));
+
+    return loggedInDomain;
+  }
+}
